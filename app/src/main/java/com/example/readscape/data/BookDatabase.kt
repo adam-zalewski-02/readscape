f5c89e5ca0e5254633b5e8167b982045cf8fff21
adam.zalewski <adam.zalewski@student.howest.be>
2023-06-10 16:02:08 +0200

    add abstract fun userDao to BookDatabase

--- app/src/main/java/com/example/readscape/data/BookDatabase.kt
+++ app/src/main/java/com/example/readscape/data/BookDatabase.kt
@@ -5,9 +5,11 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.readscape.model.User
+import com.example.readscape.model.UserDao
 
 @Database(entities = [User::class ], version = 1)
 abstract class BookDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
     companion object {
         @Volatile
         private var INSTANCE: BookDatabase? = null
