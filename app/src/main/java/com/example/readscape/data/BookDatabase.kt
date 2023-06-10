49067509e6ec88e3380b9db7df37c4cefc99b4c0
adam.zalewski <adam.zalewski@student.howest.be>
2023-06-10 14:43:55 +0200

    add UserDao interface and Bookdatabase class

--- app/src/main/java/com/example/readscape/data/BookDatabase.kt
+++ app/src/main/java/com/example/readscape/data/BookDatabase.kt
@@ -1,5 +1,27 @@
 package com.example.readscape.data
 
-class BookDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.readscape.model.User
 
+@Database(entities = [User::class ], version = 1)
+abstract class BookDatabase : RoomDatabase() {
+    companion object {
+        @Volatile
+        private var INSTANCE: BookDatabase? = null
+
+        fun getInstance(context: Context): BookDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    BookDatabase::class.java,
+                    "book_database"
+                ).fallbackToDestructiveMigration()
+                    .build()
+                    .also { INSTANCE = it }
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/example/readscape/model/UserDao.kt
@@ -0,0 +1,22 @@
+package com.example.readscape.model
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM users")
+    suspend fun getAllUsers(): List<User>
+
+    @Query("SELECT * FROM users WHERE id = :userId")
+    suspend fun getUserById(userId: Long): User?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUsers(vararg user: User)
+
+    @Delete
+    suspend fun deleteUser(user: User)
+}
