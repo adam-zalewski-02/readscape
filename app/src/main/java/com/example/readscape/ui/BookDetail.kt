11acd61c9f8de9e34627a4ab009f74d712b49cd2
adam.zalewski <adam.zalewski@student.howest.be>
2023-06-11 23:26:35 +0200

    add scrollable bookdetail back

--- app/src/main/java/com/example/readscape/ui/BookDetail.kt
+++ app/src/main/java/com/example/readscape/ui/BookDetail.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.Context
 import android.net.Uri
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -24,22 +25,27 @@ fun BookDetailScreen(book: VolumeInfo) {
 
     Box(modifier = Modifier.fillMaxSize()) {
         // Content of the book detail screen
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = book.title, fontWeight = FontWeight.Bold, fontSize = 20.sp)
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = book.authors.toString(), fontSize = 16.sp)
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(text = "Description:", fontWeight = FontWeight.Bold, fontSize = 16.sp)
-            book.description?.let { Text(text = it, fontSize = 14.sp) }
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp)
+        ) {
+            item {
+                Text(text = book.title, fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = book.authors.toString(), fontSize = 16.sp)
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "Description:", fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                book.description?.let { Text(text = it, fontSize = 14.sp) }
 
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = { shareBookTitle(book.title, context) }) {
-                Text(text = "Share")
-            }
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(onClick = { shareBookTitle(book.title, context) }) {
+                    Text(text = "Share")
+                }
 
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = { googleSearchBookTitle(book.title, context) }) {
-                Text(text = "Google Search")
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(onClick = { googleSearchBookTitle(book.title, context) }) {
+                    Text(text = "Google Search")
+                }
             }
         }
     }
